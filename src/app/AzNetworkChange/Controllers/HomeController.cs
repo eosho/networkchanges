eb06a702cc0ac8ceea58d504d50242bd5af9d6d2
Ernest Oshokoya <eoshokoya@kpmg.com>
2021-10-05 10:54:57 -0400

    push

--- src/app/AzNetworkChange/Controllers/HomeController.cs
+++ src/app/AzNetworkChange/Controllers/HomeController.cs
@@ -6,46 +6,46 @@ using Newtonsoft.Json;
 
 namespace AzNetworkChange.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    //Replace Your Azure storage Information
+
+    string StorageName = "";
+    string StorageKey = "";
+    string TableName = "networkchanges";
+
+    public IActionResult Index()
     {
-        //Replace Your Azure storage Information
+      string jsonData;
+      AzureTables.GetAllEntity(StorageName, StorageKey, TableName, out jsonData);
+      NetworkChangeModel changeModel = JsonConvert.DeserializeObject<NetworkChangeModel>(jsonData);
 
-        string StorageName = "";
-        string StorageKey = "";
-        string TableName = "";
-
-        public IActionResult Index()
-        {
-            string jsonData;
-            AzureTables.GetAllEntity(StorageName, StorageKey, TableName, out jsonData);
-            NetworkChangeModel changeModel = JsonConvert.DeserializeObject<NetworkChangeModel>(jsonData);
-
-            return View(changeModel);
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      return View(changeModel);
     }
+
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
+
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- src/app/AzNetworkChange/Models/NetworkChangeModel.cs
+++ src/app/AzNetworkChange/Models/NetworkChangeModel.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 
 namespace AzNetworkChange.Models
 {
-    public class NetworkChangeModel
-    {
-        public List<NetworkChangeDetails> Value { get; set; }
-    }
+  public class NetworkChangeModel
+  {
+    public List<NetworkChangeDetails> Value { get; set; }
+  }
 
-    public class NetworkChangeDetails
-    {
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
-        public DateTime Timestamp { get; set; }
-        public string Operation { get; set; }
-        public string Subscription { get; set; }
-        public string ResourceId { get; set; }
-        public string ChangedProperty { get; set; }
-        public string ResourceType { get; set; }
-        public string BeforeValue { get; set; }
-        public string AfterValue { get; set; }
-        public string ChangedBy {get; set; }
-        public string RawPayload { get; set; }
-        public DateTime time { get; set; }
-        public DateTime DateTime { get; set; }
+  public class NetworkChangeDetails
+  {
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Operation { get; set; }
+    public string Subscription { get; set; }
+    public string ResourceId { get; set; }
+    public string ChangedProperty { get; set; }
+    public string ResourceType { get; set; }
+    public string BeforeValue { get; set; }
+    public string AfterValue { get; set; }
+    public string ChangedBy { get; set; }
+    public string RawPayload { get; set; }
+    public DateTime time { get; set; }
+    public DateTime DateTime { get; set; }
 
-    }
+  }
 }
